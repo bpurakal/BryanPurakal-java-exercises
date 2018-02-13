4f8537a9fa8f19ef213b469db2f897abbcdb00dc
Bryan Purakal <bpurakal@gmail.com>
2018-02-13 01:12:27 -0500

    w5d1 hw

--- m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FileSplitter.java
+++ m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FileSplitter.java
@@ -1,10 +1,45 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FileSplitter {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		File inputFile = getInputFileFromUser();
+		Scanner userFile = new Scanner(System.in);
+		
+		System.out.println("Please enter the amount of lines per file: ");
+		String stringLinesPerFile = userFile.nextLine();
+		int linesPerFile = Integer.parseInt(stringLinesPerFile);
+		
+		int fileSequenceNumber = 1;
+		for (int i = 0; i <linesPerFile; i++) {
+			File newFile = new File("Shortened Text File " + fileSequenceNumber);
+			newFile.createNewFile();
+			try(Scanner fileScanner = new Scanner(inputFile); PrintWriter writer = new PrintWriter(newFile)) {
+				for (i=0; i<linesPerFile; i++)
+				writer.println(fileScanner.nextLine());
+			}
+			if (i==linesPerFile) {
+				fileSequenceNumber +=1;
+				newFile = new File("Shortened Text File " + fileSequenceNumber);
+				newFile.createNewFile();
+			}
+		}
 
 	}
+	
+	
+	@SuppressWarnings("resource")
+	private static File getInputFileFromUser() {
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Please enter path to input file >>>");
+		String path = userInput.nextLine();
+		return null;
+	}
+	
 
 }
--- m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FizzWriter.java
+++ m1-w4d3-file-io-part2-exercises/src/main/java/com/techelevator/FizzWriter.java
@@ -1,10 +1,36 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class FizzWriter {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException  {
 
+		try (PrintWriter writer = new PrintWriter("FizzBuzz.txt")) {
+
+			for (int number = 1; number <= 300; number++) {
+
+				if ((number % 3 == 0) && (number % 5 == 0)) {
+					writer.println("FizzBuzz");
+				} else if (number % 3 == 0) {
+					writer.println("Fizz");
+				} else if (number % 5 == 0) {
+					writer.println("Buzz");
+				} else {
+					writer.println(Integer.toString(number));
+				}
+
+			}
+
+	} 
+//			catch (Exception e) {
+//			System.out.println("File not found");
+//			System.exit(1);
+//		}
 	}
 
 }
