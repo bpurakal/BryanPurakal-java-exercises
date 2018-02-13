b23a51ad44e0fed46b2754f8b7d63a6027019d31
Bryan Purakal <bpurakal@gmail.com>
2018-02-13 10:19:12 -0500

    more corrections w5d1 finish

--- m2-w5d1-intro-to-databases-exercises/intro-to-databases-exercises.sql
+++ m2-w5d1-intro-to-databases-exercises/intro-to-databases-exercises.sql
@@ -31,9 +31,9 @@
 -- 13. The name of all cities in the USA with a population between 1 million and 2 million people (6 rows) 
         SELECT name FROM city WHERE countrycode = 'USA' AND population BETWEEN 1000000 AND 2000000;
 -- 14. The name and region of all countries in North or South America except for countries in the Caribbean (27 rows)
-        SELECT name, region FROM country WHERE continent = '%America' AND region != 'Caribbean'; 
+        SELECT name, region FROM country WHERE continent LIKE '%America' AND region != 'Caribbean'; 
 -- 15. The name, population, and GNP of all countries with a GNP greater than $1 trillion dollars and a population of less than 100 million people (4 rows)
-        SELECT name, population, gnp FROM country WHERE gnp > 1000000 AND population < 100000000; --i think you meant million dollars
+        SELECT name, population, gnp FROM country WHERE gnp > 1000000 AND population < 100000000; 
 -- 16. The name and population of all cities in Texas that have a population of greater than 1 million people (3 rows)
         SELECT name, population FROM city WHERE population > 1000000 AND district = 'Texas';
 -- 17. The name and average life expectancy of all countries on the continent of Oceania (28 rows)
